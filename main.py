a43ec91f5c75fbc91d0e8df89a12a95d6018b3ea
Juan Pablo Saluzzo <saluzzojuampi@gmail.com>
2023-02-12 22:56:23 -0300

    main pantalla inicial

--- main.py
+++ main.py
@@ -7,3 +7,26 @@
     - Crear nota, mostrar notas o borrarlas.
 """
 
+print("""
+
+    Acciones disponibles:
+        - registro
+        - login
+""")
+
+accion = input("Elije una opcion: ")
+
+if accion == "registro":
+    print("\nIniciando registro en el sistema...")
+    nombre = input("Ingrese su nombre: ")
+    apellido = input("Ingrese su apellido: ")
+    correo = input("Ingrese su correo electronico: ")
+    password = input("Ingrese su contrasenia:")
+    
+elif accion == "login":
+    print("\nIniciando logueo en el sistema...")
+    correo = input("Ingrese su correo electronico: ")
+    password = input("Ingrese su contrasenia: ")
+    
+
+  
